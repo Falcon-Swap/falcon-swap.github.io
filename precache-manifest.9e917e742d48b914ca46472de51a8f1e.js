e7ca85ef0a1caf068c42b9af29011a4503558079
falcon <falcon@gmail.com>
2021-08-18 05:12:12 -0700

    init

---
+++ precache-manifest.9e917e742d48b914ca46472de51a8f1e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "450cfce8ae54e5617673",
+    "url": "/static/css/main.2eef2396.chunk.css"
+  },
+  {
+    "revision": "450cfce8ae54e5617673",
+    "url": "/static/js/main.450cfce8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "1b835660bc314d067374",
+    "url": "/static/css/2.a5b9361b.chunk.css"
+  },
+  {
+    "revision": "1b835660bc314d067374",
+    "url": "/static/js/2.1b835660.chunk.js"
+  },
+  {
+    "revision": "0bb5944b5a49a9671b142b0c8fee9ae9",
+    "url": "/static/media/logo.0bb5944b.png"
+  },
+  {
+    "revision": "c9b44bec411ca66ec4ab43fc55ae2a8b",
+    "url": "/static/media/wpen.c9b44bec.pdf"
+  },
+  {
+    "revision": "fab38f0f3133627647316ecd4c43150d",
+    "url": "/static/media/wpzh_CN.fab38f0f.pdf"
+  },
+  {
+    "revision": "0eb504c549d853605958290e4a42a3fc",
+    "url": "/static/media/medium.0eb504c5.png"
+  },
+  {
+    "revision": "65b37d1b4a29d6b7371fe5322d7aa14e",
+    "url": "/static/media/twitter.65b37d1b.png"
+  },
+  {
+    "revision": "2fe67a61f25fd6a07b9897102e1591d7",
+    "url": "/index.html"
+  }
+];
