2a6f4ee2552d7d8b61fe1db9d84fcf25654a6508
falcon <falcon@gmail.com>
2021-08-22 07:49:33 -0700

    update for test

--- precache-manifest.98fdf17f3d6dbf771d5c710b540d531b.js
+++ precache-manifest.98fdf17f3d6dbf771d5c710b540d531b.js
@@ -1,35 +1,35 @@
 self.__precacheManifest = [
   {
-    "revision": "601553bbba050b4f75fc",
-    "url": "/static/css/main.2eef2396.chunk.css"
+    "revision": "5f006b5572316332359c",
+    "url": "/static/css/main.931525a3.chunk.css"
   },
   {
-    "revision": "601553bbba050b4f75fc",
-    "url": "/static/js/main.601553bb.chunk.js"
+    "revision": "5f006b5572316332359c",
+    "url": "/static/js/main.5f006b55.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.42ac5946.js"
   },
   {
-    "revision": "2435dc93cdce30ddd8e0",
+    "revision": "2c6e61f61f2a111fd5e2",
     "url": "/static/css/2.a5b9361b.chunk.css"
   },
   {
-    "revision": "2435dc93cdce30ddd8e0",
-    "url": "/static/js/2.2435dc93.chunk.js"
+    "revision": "2c6e61f61f2a111fd5e2",
+    "url": "/static/js/2.2c6e61f6.chunk.js"
   },
   {
     "revision": "0bb5944b5a49a9671b142b0c8fee9ae9",
     "url": "/static/media/logo.0bb5944b.png"
   },
   {
-    "revision": "c9b44bec411ca66ec4ab43fc55ae2a8b",
-    "url": "/static/media/wpen.c9b44bec.pdf"
+    "revision": "ca7f3ba826e7907ad2186474dc698728",
+    "url": "/static/media/wpen.ca7f3ba8.pdf"
   },
   {
-    "revision": "fab38f0f3133627647316ecd4c43150d",
-    "url": "/static/media/wpzh_CN.fab38f0f.pdf"
+    "revision": "26ab52464c5a21d25c51e7ccae728b9a",
+    "url": "/static/media/wpzh_CN.26ab5246.pdf"
   },
   {
     "revision": "0eb504c549d853605958290e4a42a3fc",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/static/media/twitter.65b37d1b.png"
   },
   {
-    "revision": "0a830188c987b2e631156dde18c94d97",
+    "revision": "f8a1a84a02342ce9d7f693412feecc12",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.955b93530153ad206c1808c504882aac.js"
+  "/precache-manifest.98fdf17f3d6dbf771d5c710b540d531b.js"
 );
 
 workbox.clientsClaim();
